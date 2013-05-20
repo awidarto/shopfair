261fb5ed03b64f245a0dda8f6d8e58e472656bf1
taufiqridha <taufiq.ridha@gmail.com>
2013-05-20 12:22:32 +0700

    shop push 10

--- application/views/articles/read.blade.php
+++ application/views/articles/read.blade.php
@@ -3,8 +3,14 @@
 @section('content')
 
 <h3>{{$article['title']}}</h3>
+<?php
+$newdir = realpath(Config::get('kickstart.storage')).'/articles/'.$article["_id"];
+if(file_exists($newdir)){?>
+<img src="{{ URL::base().'/storage/articles/'.$article['_id'].'/art_pic0'.$article['defaultpic'].'.jpg' }}" alt="{{ $article['title']}}" class=""  />			
+<?php
+}
+?>
 
-<img src="{{ URL::base().'/storage/articles/'.$article['_id'].'/art_pic0'.$article['defaultpic'].'.jpg' }}" alt="{{ $article['title']}}" class=""  />
 {{ $article['bodycopy']}}
 	
 
