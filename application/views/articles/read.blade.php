40f3d59b3dde90aeb3c79b1196baad61206e80d6
taufiqridha <taufiq.ridha@gmail.com>
2013-05-20 12:25:53 +0700

    shop push 12

--- application/views/articles/read.blade.php
+++ application/views/articles/read.blade.php
@@ -4,7 +4,7 @@
 
 <h3>{{$article['title']}}</h3>
 <?php
-$newdir = realpath(Config::get('kickstart.storage')).'/articles/'.$article["_id"].'art_pic0'.$article['defaultpic'].'.jpg';
+$newdir = realpath(Config::get('kickstart.storage')).'/articles/'.$article["_id"].'/art_pic0'.$article['defaultpic'].'.jpg';
 if(file_exists($newdir)){?>
 <img src="{{ URL::base().'/storage/articles/'.$article['_id'].'/art_pic0'.$article['defaultpic'].'.jpg' }}" alt="{{ $article['title']}}" class=""  />			
 <?php
