10c856abd99d08f74f026dbb156fba6d3e1593a8
taufiqridha <taufiq.ridha@gmail.com>
2013-05-20 12:27:29 +0700

    shop push 13

--- application/views/partials/festivalhome.blade.php
+++ application/views/partials/festivalhome.blade.php
@@ -24,7 +24,7 @@
       <div class="large-4 columns ">
         {{ HTML::image('images/gallery-ico.png','',array('class'=>'logo-header')) }}<br/>
         <h3 class="subtitlefestival">GALLERY</h3>
-        Whether you're packing for a short work trip or an extended getaway,<br/><a class="more" href="#">more</a>
+        Whether you're packing for a short work trip or an extended getaway,<br/><a class="more" href="{{ URL::to('reader/article/gallery') }}">more</a>
       </div>
       <div class="large-4 columns ">
         {{ HTML::image('images/music-ico.png','',array('class'=>'logo-header')) }}<br/>
