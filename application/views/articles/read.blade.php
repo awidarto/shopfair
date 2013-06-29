5c3e24752adcc788fdfcfc364d3032e55df89b5e
Andy Awidarto <andy.awidarto@gmail.com>
2013-06-29 16:42:39 +0700

    poster home

--- application/views/articles/read.blade.php
+++ application/views/articles/read.blade.php
@@ -6,13 +6,11 @@
 <?php
 $newdir = realpath(Config::get('kickstart.storage')).'/articles/'.$article["_id"].'/art_pic0'.$article['defaultpic'].'.jpg';
 if(file_exists($newdir)){?>
-<img src="{{ URL::base().'/storage/articles/'.$article['_id'].'/art_pic0'.$article['defaultpic'].'.jpg' }}" alt="{{ $article['title']}}" class=""  />			
+<img src="{{ URL::base().'/storage/articles/'.$article['_id'].'/poster_land_pic0'.$article['defaultpic'].'.jpg' }}" alt="{{ $article['title']}}" class=""  />			
 <?php
 }
 ?>
 
 {{ $article['bodycopy']}}
 	
-
-
 @endsection
--- application/views/inner.blade.php
+++ application/views/inner.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <title>{{ Config::get('site.title')}}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
@@ -23,100 +23,114 @@
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
       <script src="../assets/js/html5shiv.js"></script>
-    <![endif]-->
+      <![endif]-->
 
-    <!-- Fav and touch icons -->
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
-    <link rel="shortcut icon" href="../assets/ico/favicon.png">
+      <!-- Fav and touch icons -->
+      <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+      <link rel="shortcut icon" href="../assets/ico/favicon.png">
 
 
 
-  </head>
-  <body>
-    <div class="row headercontainer show-for-medium-up" style="width:1168px;">
-      <div class="large-12 columns">
-     
+</head>
+<body>
+<div class="row headercontainer show-for-medium-up" style="width:1168px;">
+    <div class="large-12 columns">
+
         <!-- Navigation -->
-          @yield('topnav')
-     
+        @yield('topnav')
+
         <!-- End Navigation -->
 
-        </div>
-      </div>
-     
-    <div class="row headercontainer show-for-small">
-        <div class="large-12 columns">
+    </div>
+</div>
+
+<div class="row headercontainer show-for-small">
+    <div class="large-12 columns">
         <!-- Navigation -->
-          @yield('topnav')     
+        @yield('topnav')     
         <!-- End Navigation -->
+    </div>
+</div>
+
+<div class="row">
+    <div class="large-3 columns  show-for-medium-up">
+        <div class="boxcontainer">
+            <div class="box-inner">
+                <h4>SUBSCRIBE</h4>
+                <input type="text" name="usrname">
+                <input type="submit" value="SUBSCRIBE" class="submitsubscribe">
+                <span class="descbox">Subscribe to keep updated with Shopfair latest news.</span>
+            </div>
+
+            <div class="box-inner">
+                <h4>CONNECT</h4>
+                {{ HTML::image('images/connect.png','shopfair',array('class'=>'')) }}
+            </div>
         </div>
     </div>
 
-      <div class="row">
-        <div class="large-3 columns">
-          <div class="boxcontainer">
+    <div class="contentinner large-9 columns">
+        @yield('content')    
+    </div>
+
+    <div class="large-3 columns show-for-small">
+        <div class="boxcontainer">
             <div class="box-inner">
-              <h4>SUBSCRIBE</h4>
-              <input type="text" name="usrname">
-              <input type="submit" value="SUBSCRIBE" class="submitsubscribe">
-              <span class="descbox">Subscribe to keep updated with Shopfair atest news.</span>
+                <h4>SUBSCRIBE</h4>
+                <input type="text" name="usrname">
+                <input type="submit" value="SUBSCRIBE" class="submitsubscribe">
+                <span class="descbox">Subscribe to keep updated with Shopfair latest news.</span>
             </div>
 
             <div class="box-inner">
-              <h4>CONNECT</h4>
-              {{ HTML::image('images/connect.png','shopfair',array('class'=>'')) }}
+                <h4>CONNECT</h4>
+                {{ HTML::image('images/connect.png','shopfair',array('class'=>'')) }}
             </div>
-          </div>
-
         </div>
+    </div>
 
-        <div class="contentinner large-9 columns">
-          @yield('content')    
-        </div>
-      </div>
-      
-     
+</div>
+<!--
+    @yield('sponsorshome')
+-->
 
 
-      @yield('sponsorshome')
-     
-     
-      <!-- Footer -->
-      @yield('footer')
-     
+<!-- Footer -->
+@yield('footer')
 
-    
 
-    {{ HTML::script('js/jquery-ui-1.9.2.custom.min.js') }}
-  
-    {{ HTML::script('js/jquery.dataTables.min.js') }}
 
-    {{ HTML::script('js/foundation/foundation/foundation.js') }}
-    {{ HTML::script('js/foundation/foundation/foundation.alerts.js') }}
-    {{ HTML::script('js/foundation/foundation/foundation.clearing.js') }}
-    {{ HTML::script('js/foundation/foundation/foundation.cookie.js') }}
-    {{ HTML::script('js/foundation/foundation/foundation.dropdown.js') }}
-    {{ HTML::script('js/foundation/foundation/foundation.forms.js') }}
-    {{ HTML::script('js/foundation/foundation/foundation.joyride.js') }}
-    {{ HTML::script('js/foundation/foundation/foundation.magellan.js') }}
-    {{ HTML::script('js/foundation/foundation/foundation.orbit.js') }}
-    {{ HTML::script('js/foundation/foundation/foundation.placeholder.js') }}
-    {{ HTML::script('js/foundation/foundation/foundation.reveal.js') }}
-    {{ HTML::script('js/foundation/foundation/foundation.section.js') }}
-    {{ HTML::script('js/foundation/foundation/foundation.tooltips.js') }}
-    {{ HTML::script('js/foundation/foundation/foundation.topbar.js') }}
 
-    
+{{ HTML::script('js/jquery-ui-1.9.2.custom.min.js') }}
 
-    {{ HTML::script('js/jquery.smartWizard-2.0.js') }}
-    <script>
-    $(document).foundation();
-    </script>
+{{ HTML::script('js/jquery.dataTables.min.js') }}
 
-    {{ HTML::script('js/jquery.simplecolorpicker.js') }}
+{{ HTML::script('js/foundation/foundation/foundation.js') }}
+{{ HTML::script('js/foundation/foundation/foundation.alerts.js') }}
+{{ HTML::script('js/foundation/foundation/foundation.clearing.js') }}
+{{ HTML::script('js/foundation/foundation/foundation.cookie.js') }}
+{{ HTML::script('js/foundation/foundation/foundation.dropdown.js') }}
+{{ HTML::script('js/foundation/foundation/foundation.forms.js') }}
+{{ HTML::script('js/foundation/foundation/foundation.joyride.js') }}
+{{ HTML::script('js/foundation/foundation/foundation.magellan.js') }}
+{{ HTML::script('js/foundation/foundation/foundation.orbit.js') }}
+{{ HTML::script('js/foundation/foundation/foundation.placeholder.js') }}
+{{ HTML::script('js/foundation/foundation/foundation.reveal.js') }}
+{{ HTML::script('js/foundation/foundation/foundation.section.js') }}
+{{ HTML::script('js/foundation/foundation/foundation.tooltips.js') }}
+{{ HTML::script('js/foundation/foundation/foundation.topbar.js') }}
 
-  </body>
+
+
+{{ HTML::script('js/jquery.smartWizard-2.0.js') }}
+<script>
+$(document).foundation();
+</script>
+
+{{ HTML::script('js/jquery.simplecolorpicker.js') }}
+
+</body>
 </html>
